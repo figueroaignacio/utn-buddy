3cbdf4c1207768ef1c8f8a4585a68eec3ca3dd37
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-03-28 22:00:34 -0300

    remove all to make this sh*t in python

--- apps/web/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import { config } from '@repo/eslint-config/vite-react';
-
-/** @type {import("eslint").Linter.Config[]} */
-export default config;
--- apps/web/src/App.tsx
+++
@@ -1,7 +0,0 @@
-export function App() {
-  return (
-    <main>
-      <h1>UTN Buddy</h1>
-    </main>
-  );
-}
